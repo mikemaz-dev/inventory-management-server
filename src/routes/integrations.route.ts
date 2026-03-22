0263cfdd56299a045563218b4bf6a8d5dd01e7bb
Mikhail Mazurkevich <mazurkevich.mikhail.14@gmail.com>
2026-03-22 19:12:23 +0300

    change checking role in integration route

--- src/routes/integrations.route.ts
+++ src/routes/integrations.route.ts
@@ -6,7 +6,7 @@ const router = Router()
 
 router.post(
 	'/salesforce/user',
-	authMiddleware('USER'),
+	authMiddleware(),
 	salesForceController.createIntegration.bind(salesForceController),
 )
 export const integrationsRouter = router
